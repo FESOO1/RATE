edc1bb7ab313789d257654545f8a98004bad3c4e
FES001 <firdavs7794948@gmail.com>
2024-05-01 00:30:29 +0500

    s

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-const numbersText = document.querySelectorAll('numbers');
+// RATE
+
+const submitButton = document.querySelector('.submit');
+const rateIself = document.querySelector('.rate-itself');
+const rateSuccess = document.querySelector('.rate-success');
+
+submitButton.addEventListener('click', () => {
+    rateIself.style.display = 'none';
+    rateSuccess.style.display = 'flex';
+})
+
+// SELECTED NUMBER
+
+let selectedNumber;
+const selectedNumberText = document.getElementById('selectedNumber');
+const numberOne = document.getElementById('numberOne');
+const numberTwo = document.getElementById('numberTwo');
+const numberThree = document.getElementById('numberThree');
+const numberFour = document.getElementById('numberFour');
+const numberFive = document.getElementById('numberFive');
+
+numberOne.addEventListener('click', One)
+numberTwo.addEventListener('click', Two)
+numberThree.addEventListener('click', Three)
+numberFour.addEventListener('click', Four)
+numberFive.addEventListener('click', Five)
+
+function One(){
+    selectedNumber = numberOne.innerText;
+    selectedNumberText.textContent = selectedNumber;
+    numberOne.classList.add('selected');
+}
+
+function Two(){
+    selectedNumber = numberTwo.innerText;
+    selectedNumberText.textContent = selectedNumber;
+    numberTwo.classList.add('selected');
+}
+
+function Three(){
+    selectedNumber = numberThree.innerText;
+    selectedNumberText.textContent = selectedNumber;
+    numberThree.classList.add('selected');
+}
+
+function Four(){
+    selectedNumber = numberFour.innerText;
+    selectedNumberText.textContent = selectedNumber;
+    numberFour.classList.add('selected');
+}
+
+function Five(){
+    selectedNumber = numberFive.innerText;
+    selectedNumberText.textContent = selectedNumber;
+    numberFive.classList.add('selected');
+}
